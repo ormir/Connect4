784df9e752560a4d6e075873e2dfd0e561d603fa
Ormir G <ormirgj@gmail.com>
2016-04-20 11:26:53 +0200

    Init

---
+++ main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  Connect4
+//
+//  Copyright © 2016 Ormir Gjurgjejt, Nitika Kumar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
