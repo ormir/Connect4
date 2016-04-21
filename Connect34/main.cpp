aa6d006449d640a30e3fcfd14e39a822c6cbea48
Ormir G <ormirgj@gmail.com>
2016-04-21 17:29:20 +0200

    Initial Commit

---
+++ Connect34/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Connect34
+//
+//  Created by Ormir G. on 21/04/16.
+//  Copyright © 2016 Ormir Gjurgjejt, Nitika Kumar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
